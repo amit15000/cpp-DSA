65148df4a92d5918472b65bedfdba0032e93b441
Amit Kumar <15000amitkumar@gmail.com>
2023-10-24 01:13:28 +0530

    Deletion of nodes

--- Linked List/singly_linked_list.cpp
+++ Linked List/singly_linked_list.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// New branch --> Linked_List
 class Node
 {
 public:
@@ -22,7 +21,7 @@ public:
     }
 };
 
-void *InsertAtHead(Node *(&head), int _val)
+void InsertAtHead(Node *(&head), Node *(&tail), int _val)
 {
 
     // if (head == NULL)
@@ -38,10 +37,18 @@ void *InsertAtHead(Node *(&head), int _val)
 
     // this much is also enough
     Node *newNode = new Node(_val);
+    // its first node of the linked list
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
     newNode->next = head;
     head = newNode;
 }
-void InsertAtTail(Node *(head), int _val)
+void InsertAtTail_using_head(Node *(head), int _val)
 {
 
     while (head->next != NULL)
@@ -52,6 +59,97 @@ void InsertAtTail(Node *(head), int _val)
 
     head->next = newNode;
     head = newNode;
+}
+void InsertAtTail_using_tail(Node *(&head), Node *(&tail), int _val)
+{
+    Node *newNode = new Node(_val);
+    // its first node of the linked list
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+
+    tail = newNode;
+}
+void DeleteNodeFromHead(Node *(&head))
+{
+    Node *temp = head;
+    head = head->next;
+
+    delete temp;
+}
+void DeleteNodeFromTail(Node *(&head), Node *(&tail))
+{
+    Node *temp = head;
+    while (temp->next != tail)
+    {
+        temp = temp->next;
+    }
+
+    delete tail;
+    tail = temp;
+    tail->next = NULL;
+}
+int len_of_list(Node *head)
+{
+    int len = 0;
+    while (head != NULL)
+    {
+        head = head->next;
+        len++;
+    }
+    return len;
+}
+void DeleteNode(int p, Node *(&head), Node *(&tail))
+{
+    if (head == NULL)
+    {
+        cout << "There is no node present in the linked list" << endl;
+        return;
+    }
+
+    // delete from head
+
+    // case 1  there is only 1 node (head) is present
+    if (head == tail)
+    {
+        delete head;
+        head = NULL;
+        tail = NULL;
+        return;
+    }
+
+    // case 2 delete from head
+    if (p <= 0)
+    {
+        DeleteNodeFromHead(head);
+        return;
+    }
+
+    int len = len_of_list(head);
+    // case 3 delete from tail
+    if (p >= len)
+    {
+        DeleteNodeFromTail(head, tail);
+        return;
+    }
+
+    // Case 4   -- deletion- in - between
+    Node *tempP = head;
+    int i = 1;
+    while (i < p - 1)
+    {
+        tempP = tempP->next;
+        i++;
+    }
+    Node *tempCurrent = tempP->next;
+    // Node *tempNN = tempCurrent->next;  //trying without it
+    tempP->next = tempCurrent->next;
+    delete tempCurrent;
 }
 void printLinkedList(Node *(head))
 {
@@ -67,18 +165,28 @@ int main()
 {
     Node *head = NULL;
     Node *tail = head;
-    InsertAtHead(head, 112);
-    InsertAtHead(head, 113);
-    InsertAtTail(head, 12);
-    InsertAtTail(head, -5);
-    InsertAtTail(head, 45);
-    InsertAtTail(head, 78);
-    InsertAtTail(head, 34);
+    InsertAtHead(head, tail, 10);
+    InsertAtHead(head, tail, 2);
+    InsertAtHead(head, tail, 40);
+    InsertAtHead(head, tail, 113);
+    InsertAtTail_using_tail(head, tail, 20);
+    InsertAtTail_using_tail(head, tail, 30);
+    // InsertAtTail_using_tail(head, tail, 40);
 
-    // InsertAtHead(head, 345);
-    // InsertAtHead(head, 43);
-    // InsertAtHead(head, 7);
-    // InsertAtHead(head, 11);
     printLinkedList(head);
+    DeleteNode(3, head, tail);
+    printLinkedList(head);
+    DeleteNode(4, head, tail);
+    printLinkedList(head);
+    DeleteNode(4, head, tail);
+    printLinkedList(head);
+    DeleteNode(0, head, tail);
+    printLinkedList(head);
+    DeleteNode(0, head, tail);
+    printLinkedList(head);
+    DeleteNode(0, head, tail);
+    printLinkedList(head);
+    DeleteNode(3, head, tail);
+
     return 0;
 }
