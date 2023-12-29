d82d8fe67939b1e63851efed563f1b2dd9c19414
Amit Kumar <15000amitkumar@gmail.com>
2023-12-29 19:28:58 +0530

    Queue Operations varified

--- Queue/queue.cpp
+++ Queue/queue.cpp
@@ -2,4 +2,13 @@
 using namespace std;
 int main()
 {
+    Queue q(5);
+    q.push(12);
+    q.push(4);
+    q.push(-7);
+    q.push(9);
+    q.push(9);
+    q.push(9);
+    q.push(9);
+    q.push(9);
 }
