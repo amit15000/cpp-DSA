3d7e55d1db7f800abd75f7cd6565ce476bf2eaa3
Amit Kumar <15000amitkumar@gmail.com>
2024-02-12 15:07:35 +0530

    Search in Trie

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -62,6 +62,63 @@ public:
     {
         insertUtil(root, word);
     }
+
+    bool searchUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            // terminal ki value 1 hai --> word present   ==> return value of termianl that is 1 otherwise terminal val = 0
+            return root->isTerminal;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode *child = nullptr;
+        // present
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            // not present
+            return 0;
+        }
+
+        // recursion
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool searchWord(string word)
+    {
+        return searchUtil(root, word);
+    }
+
+    void removeUtil(TrieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            root->isTerminal = false;
+            return;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode *child = nullptr;
+        if (root->children[index] != nullptr)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            cout << "Word Already Not Present" << endl;
+            return;
+        }
+        removeUtil(child, word.substr(1));
+    }
+
+    void removeWord(string word)
+    {
+        removeUtil(root, word);
+    }
 };
 int main()
 {
@@ -69,6 +126,32 @@ int main()
     Trie t;
     t.insertWord("AMIT");
     t.insertWord("AMYTU");
+    t.insertWord("TIMER");
+    t.insertWord("TIM");
 
+    // // search word
+    bool x = t.searchWord("TIM");
+    if (x)
+    {
+        cout << "Present" << endl;
+    }
+    else
+    {
+        cout << "Absent" << endl;
+    }
+
+    // remove word
+    t.removeWord("TIM");
+
+    // search again
+    x = t.searchWord("TIM");
+    if (x)
+    {
+        cout << "Present" << endl;
+    }
+    else
+    {
+        cout << "Absent" << endl;
+    }
     return 0;
 }
