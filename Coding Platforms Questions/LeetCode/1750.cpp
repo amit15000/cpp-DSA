7e50fc3885f721f6a833eb31f574bf6670abe9c6
Amit Kumar <15000amitkumar@gmail.com>
2024-03-05 19:53:27 +0530

    1750

---
+++ Coding Platforms Questions/LeetCode/1750.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i=0;
+        int j=s.length()-1;
+    }
+};
+int main(){
+    string s="acabaabac";
+    int size = s.length();
+    cout<<size<<endl;
+    Solution c;
+    cout<<c.minimumLength(s)<<endl;
+    
+}
