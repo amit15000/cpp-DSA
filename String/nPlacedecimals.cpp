09efb4222b8fdaac13de66118fea5942e822cc2f
Amit Kumar <15000amitkumar@gmail.com>
2024-04-04 04:17:05 +0530

    fixed setprecision

--- String/nPlacedecimals.cpp
+++ String/nPlacedecimals.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    int t = 10;                                                               // Assuming t is an integer
-    t = static_cast<double>(t);                                               // Convert t to a double
-    cout << fixed << setprecision(5) << static_cast<double>(100) / 8 << endl; // Output: 10.0
+    int t = 10;                                                                // Assuming t is an integer
+    t = static_cast<double>(t);                                                // Convert t to a double
+    cout << fixed << setprecision(10) << static_cast<double>(100) / 8 << endl; // Output: 10.0
     return 0;
 }
