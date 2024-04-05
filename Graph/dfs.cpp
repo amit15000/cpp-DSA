f201e208bfceb11614a8410c1cc728e581f2ee09
Amit Kumar <15000amitkumar@gmail.com>
2024-04-05 19:01:46 +0530

    DFS of Graph

---
+++ Graph/dfs.cpp
@@ -0,0 +1,21 @@
+void helper(int start, vector<bool> &vis, vector<int> &res, vector<int> adj[])
+{
+    vis[start] = true;
+    res.push_back(start);
+    for (int adjacent : adj[start])
+    {
+        if (!vis[adjacent])
+        {
+            helper(adjacent, vis, res, adj);
+        }
+    }
+}
+
+// Function to return a list containing the DFS traversal of the graph.
+vector<int> dfsOfGraph(int V, vector<int> adj[])
+{
+    vector<bool> vis(V, false);
+    vector<int> res;
+    helper(0, vis, res, adj);
+    return res;
+}
