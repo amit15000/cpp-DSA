ba6b68bc78871322f5e5ea2b53a272e3bd7c1fe4
Amit Kumar <15000amitkumar@gmail.com>
2024-08-15 04:02:21 +0530

    72 - Approach 2

--- Coding_Platforms_Questions/LeetCode/72 Edit Distance.cpp
+++ Coding_Platforms_Questions/LeetCode/72 Edit Distance.cpp
@@ -1,3 +1,6 @@
+
+
+// Approach 1
 class Solution
 {
 public:
@@ -43,5 +46,41 @@ public:
         l2 = word2.length();
         memset(dp, -1, sizeof(dp));
         return solve(word1, word2, 0, 0);
+    }
+};
+
+// Approach 2
+
+class Solution
+{
+public:
+    // Approach 2
+    int minDistance(string s1, string s2)
+    {
+        int n = s1.length();
+        int m = s2.length();
+        vector<vector<int>> t(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < m + 1; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    t[i][j] = i + j;
+                }
+                else if (s1[i - 1] == s2[j - 1])
+                {
+                    t[i][j] = t[i - 1][j - 1];
+                }
+                else
+                {
+                    // i r d cases
+
+                    t[i][j] = 1 + min(min(t[i][j - 1], t[i - 1][j - 1]), t[i - 1][j]);
+                }
+            }
+        }
+        return t[n][m];
     }
 };
